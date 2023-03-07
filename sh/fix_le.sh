202a35d782ab4a0675016570709a17d42ef47b5f
Simon N <simonn.bluebox@gmail.com>
2023-03-07 16:51:35 +0200

    remove blank lines in file fix script.

--- sh/fix_le.sh
+++ sh/fix_le.sh
@@ -6,7 +6,6 @@ git config --global core.eol lf
 #Set autocrlf to false to stop converting between windows style (CRLF) and Unix style (LF)
 git config --global core.autocrlf false
 
-
 #Save your current files in Git, so that none of your work is lost.
 git add . -u
 git commit -m "Saving files before refreshing line endings"
@@ -16,23 +15,16 @@ rm .git/index
 #Rewrite the Git index to pick up all the new line endings.
 git reset
 
-
 #Show the rewritten, normalized files.
-
 git status
 
-
 #Add all your changed files back, and prepare them for a commit. This is your chance to inspect which files, if any, were unchanged.
-
 git add -u
 # It is perfectly safe to see a lot of messages here that read
 # "warning: CRLF will be replaced by LF in file."
 
-
 #Rewrite the .gitattributes file.
 git add .gitattributes
 
-
 #Commit the changes to your repository.
-
 git commit -m "Normalize all the line endings"
